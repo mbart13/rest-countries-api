990ce4b0551013b0d8a00b9de761dd520a093b19
Michal Bartosik <msbartosik@gmail.com>
2021-05-21 21:21:16 +0200

    add test for handling unsuccessful request

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,11 +2,17 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import * as api from './helpers/api';
 import data from 'mocks/data';
+import Country from 'models/Country';
 
 jest.mock('./helpers/api.ts');
+let mockGetCountries: jest.SpyInstance<Promise<Country[]>, []>;
+
+beforeEach(() => {
+  mockGetCountries = jest.spyOn(api, 'default');
+});
+
 describe('App component', () => {
   it('renders countries list if request succeeds', async () => {
-    const mockGetCountries = jest.spyOn(api, 'default');
     mockGetCountries.mockImplementationOnce((): Promise<any> => {
       return Promise.resolve(data);
     });
@@ -16,5 +22,16 @@ describe('App component', () => {
     const countries = await screen.findAllByTestId('country');
     expect(mockGetCountries).toBeCalledTimes(1);
     expect(countries).toHaveLength(20);
+  });
+
+  it('renders error message if request does not succeed', async () => {
+    mockGetCountries.mockImplementationOnce((): Promise<any> => {
+      return Promise.reject(data);
+    });
+
+    render(<App />);
+
+    const errorMessage = await screen.findByText(/something went wrong/i);
+    expect(errorMessage).toBeInTheDocument();
   });
 });
