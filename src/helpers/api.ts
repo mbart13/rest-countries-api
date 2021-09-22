09aa2c5486a9d2d8562b14f3e3f8f2a8b4d75b13
Michal Bartosik <msbartosik@gmail.com>
2021-09-22 19:49:10 +0200

    Free plan of api.countrylayer.com does not support HTTPS Encryption

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -3,7 +3,7 @@ import Country from 'models/Country';
 
 const { REACT_APP_ACCESS_KEY } = process.env;
 
-const baseUrl = `https://api.countrylayer.com/v2/all?access_key=${REACT_APP_ACCESS_KEY}`;
+const baseUrl = `http://api.countrylayer.com/v2/all?access_key=${REACT_APP_ACCESS_KEY}`;
 
 export const getCountries = async () => {
   const { data }: { data: Country[] } = await axios.get(baseUrl);
